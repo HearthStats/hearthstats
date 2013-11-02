625343197956d0fde5c24a7c7676756c3a490afa
Jeffrey Tong <trigun0x2@gmail.com>
2013-11-02 03:16:34 -0400

    added classes for each deck

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131101040158) do
+ActiveRecord::Schema.define(:version => 20131102071458) do
 
   create_table "arenas", :force => true do |t|
     t.datetime "created_at",                    :null => false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(:version => 20131101040158) do
     t.integer  "loses"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "class"
   end
 
   create_table "users", :force => true do |t|
