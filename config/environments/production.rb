a453bc1dd11da8dd5d74390d4b04a9d41b6b5b5a
Jeffrey Tong <trigun0x2@gmail.com>
2013-11-09 03:07:27 -0700

    profile moved up and default email changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Hearthstats::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'www.hearthstats.com' }
+  config.action_mailer.default_url_options = { :host => 'www.hearthstats.net' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
