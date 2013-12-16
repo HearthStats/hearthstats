e2ff53b3db675db967430ec486812a38922a2e53
Jeffrey Tong <trigun0x2@gmail.com>
2013-12-15 20:26:31 -0700

    carrierwave application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+require 'carrierwave'
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
