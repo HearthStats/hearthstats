b38cf1178242df89757ee30861fae1853bce3d88
Jeffrey Tong <trigun0x2@gmail.com>
2014-02-20 17:47:04 -0500

    arena run delete should not screw up matches

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -76,7 +76,7 @@ class MatchesController < ApplicationController
     @match.destroy
 
     respond_to do |format|
-      format.html { redirect_to constructeds_url }
+      format.html { redirect_to request.referer, notice: 'Match was successfully deleted.' }
       format.json { head :no_content }
     end
   end
--- app/models/arena_run.rb
+++ app/models/arena_run.rb
@@ -1,22 +1,16 @@
 class ArenaRun < ActiveRecord::Base
   attr_accessible :class, :gold, :dust, :completed, :user_id, :klass_id, :notes, :complete
-  
+
   has_many :arenas
   has_many :match_run
-  has_many :matches, :through => :match_run, :dependent => :destroy
+  has_many :matches, :through => :match_run
 
   belongs_to :klass
 
-  after_destroy :delete_all_arena
-
-  def delete_all_arena
-  	Match.destroy_all(:arena_run_id => self.id)
-  end
-  
   def num_wins
     return self.matches.where(:result_id => 1).count
   end
-  
+
   def num_losses
     return self.matches.where(:result_id => 2).count
   end
