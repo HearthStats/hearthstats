c54b105c4d192470c18977bb38e30022a4284b41
Jerome Dane <jerome.dane@gmail.com>
2014-02-21 09:22:14 -0500

    Fixed arena_runs api 2

--- app/models/arena_run.rb
+++ app/models/arena_run.rb
@@ -4,6 +4,8 @@ class ArenaRun < ActiveRecord::Base
   has_many :arenas
   has_many :match_run
   has_many :matches, :through => :match_run
+  validates :dust, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :gold, :numericality => { :greater_than_or_equal_to => 0 }
 
   belongs_to :klass
 
