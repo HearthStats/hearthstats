af108097d219cdf0f2ff895429d54393a255f470
Jeffrey Tong <trigun0x2@gmail.com>
2014-02-21 22:03:56 -0500

    guest delete also deletes records

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,13 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :guest, :userkey
   # attr_accessible :title, :body
-  has_one :profile
-  has_many :arenas
-  has_many :decks
-  has_many :constructeds
-  has_many :arena_runs
+  has_one :profile, dependent: :destroy
+  has_many :arenas, dependent: :destroy
+  has_many :decks, dependent: :destroy
+  has_many :constructeds, dependent: :destroy
+  has_many :arena_runs, dependent: :destroy
   belongs_to :tourny
-  has_many :matches
+  has_many :matches, dependent: :destroy
 
   def get_userkey
   	if self.userkey.nil?
