c3639ac8769a2329034f191c3cc7ad6234f44177
Jeffrey Tong <trigun0x2@gmail.com>
2014-02-24 08:49:55 -0500

    changed deck edit to make checkbox work

--- app/models/unique_deck.rb
+++ app/models/unique_deck.rb
@@ -1,20 +1,20 @@
 class UniqueDeck < ActiveRecord::Base
   attr_accessible :cardstring, :name, :user_id, :num_matches, :winrate, :num_users
   has_many :decks
-  
+
   has_many :cards, :through => :unique_deck_card
   has_many :unique_deck_card
-  
+
   has_many :matches, :through => :match_unique_deck
   has_many :match_unique_deck
-  
+
   before_save :update_stats
-  
+
   def update_stats
-    
+
     #remove existing cards
     self.unique_deck_card.destroy_all()
-    
+
     #update cards from cardstring
     cards = self.cardstring.split(',')
     cards.each do |cardData|
@@ -28,14 +28,14 @@ class UniqueDeck < ActiveRecord::Base
     self.num_minions = self.cards.where(:type_id => 1).count
     self.num_spells = self.cards.where(:type_id => 2).count
     self.num_weapons = self.cards.where(:type_id => 3).count
-    
+
     matches = self.matches.where('created_at >= ?', 30.days.ago)
-    
+
     self.num_users = self.decks.where(:klass_id => self.klass_id).count
     self.num_matches = self.matches.count
     self.num_wins = self.matches.where(:result_id => 1).count
     self.num_losses = self.matches.where(:result_id => 2).count
     self.winrate = self.num_matches > 0 ? (self.num_wins.to_f / self.num_matches.to_f * 100) : 0
-    
+
   end
 end
