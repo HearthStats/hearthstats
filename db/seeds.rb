4a506b2b664feea21588740ddb6b10b8357d6575
Gerard Leijdekkers <gerard@eet.nu>
2014-05-27 19:24:26 +0200

    Added season seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-
-require "#{Rails.root}/db/card_seeds.rb"
-require "#{Rails.root}/db/class_seeds.rb"
+%w(card class season).each do |object|
+  require "#{Rails.root}/db/#{object}_seeds.rb"
+end
