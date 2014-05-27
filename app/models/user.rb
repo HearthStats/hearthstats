1a75e44c3a58c329ce9eda990d9ef8bda05f31d0
Gerard Leijdekkers <gerard@eet.nu>
2014-05-27 20:13:53 +0200

    User cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,45 +1,43 @@
 class User < ActiveRecord::Base
   rolify
-	extend Mailboxer::Models::Messageable::ActiveRecord
-
+  extend Mailboxer::Models::Messageable::ActiveRecord
+  acts_as_messageable
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,:token_authenticatable
-
+         
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :guest, :userkey, :subscription_id, :authentication_token
-
+  
+  ### ASSOCIATIONS:
+  
   has_one :profile, dependent: :destroy
   has_many :arenas, dependent: :destroy
   has_many :decks, dependent: :destroy
   has_many :constructeds, dependent: :destroy
   has_many :arena_runs, dependent: :destroy
-  belongs_to :tourny
   has_many :matches, dependent: :destroy
-  belongs_to :subscription
-
-  acts_as_messageable
   has_many :team_users
   has_many :teams, through: :team_users
-
+  belongs_to :tourny
+  belongs_to :subscription
+  
+  ### INSTANCE METHODS:
+  
   def get_userkey
-  	if self.userkey.nil?
-  		self.userkey = SecureRandom.hex
-  		self.save!
-
-  		self.userkey
-  	else
-  		self.userkey
-  	end
+    update_attribute(:userkey, SecureRandom.hex) if userkey.nil?
+    
+    userkey
   end
-
+  
   def name
-	  return "You should add method :name in your Messageable model"
-	end
-
+    return "You should add method :name in your Messageable model"
+  end
+  
   def mailboxer_email(object)
-	  nil
-	end
+    nil
+  end
 
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ require 'factory_girl'
 
 FactoryGirl.define do
   factory :user do
-      email 'bobbyjoe@mailinator'
-      password 'secret'
+      email    { Faker::Internet.email       }
+      password { Faker::Internet.password(6) }
   end
 end
