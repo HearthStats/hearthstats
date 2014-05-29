1b8cdb203c2c52cc7740c800b0653ffc534ce8d2
Gerard Leijdekkers <gerard@eet.nu>
2014-05-29 07:24:09 +0200

    Do not cache classes when using a DRb server

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,8 +5,8 @@ Hearthstats::Application.configure do
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs. Don't rely on the data there!
-  config.cache_classes = true
-
+  config.cache_classes = !(ENV['DRB'] == 'true')
+  
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
