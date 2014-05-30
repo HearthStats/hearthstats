86d2768086d80e49edb2ea91e912b5f71a15cb21
Gerard Leijdekkers <gerard@eet.nu>
2014-05-30 11:36:59 +0200

    Added demo data seeds fort easier interface testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-%w(class card season match_result).each do |object|
+%w(class card season match_result demo_data).each do |object|
   require "#{Rails.root}/db/#{object}_seeds.rb"
 end
