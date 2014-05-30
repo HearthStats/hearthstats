0ce55762b11f1019dcc8dbda25c1513ee2e92565
Gerard Leijdekkers <gerard@eet.nu>
2014-05-30 12:16:29 +0200

    removed unneeded self references

--- app/models/unique_deck.rb
+++ app/models/unique_deck.rb
@@ -43,10 +43,10 @@ class UniqueDeck < ActiveRecord::Base
     self.num_minions = cards.where(type_id: 1).count
     self.num_spells  = cards.where(type_id: 2).count
     self.num_weapons = cards.where(type_id: 3).count
-    self.num_users   = self.decks.where(klass_id: klass_id).count
-    self.num_matches = self.decks.sum(:user_num_matches)
-    self.num_wins    = self.decks.sum(:user_num_wins)
-    self.num_losses  = self.decks.sum(:user_num_losses)
+    self.num_users   = decks.where(klass_id: klass_id).count
+    self.num_matches = decks.sum(:user_num_matches)
+    self.num_wins    = decks.sum(:user_num_wins)
+    self.num_losses  = decks.sum(:user_num_losses)
     self.winrate     = (num_matches.present? && num_matches != 0) ? (num_wins.to_f / num_matches.to_f * 100) : 0
   end
 end
