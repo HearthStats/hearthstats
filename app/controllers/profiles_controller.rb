d6fd4455f0d4b53eb4f56e9f09f963a36cd128fc
Jeffrey Tong <trigun0x2@gmail.com>
2014-06-07 19:53:25 -0400

    added guarding for guest users viewing new profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -44,7 +44,7 @@ class ProfilesController < ApplicationController
     end
 
     if @user.is_new?
-      if current_user.id == @user.id
+      if current_user && current_user.id == @user.id
         return redirect_to root_url, alert: "You must enter at least one game before your profile is active."
       else
         return redirect_to root_url, alert: "User profile is not active."
