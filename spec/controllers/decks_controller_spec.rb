d72ef479c2a27f936432a9218a3ed7ecbe34ade5
Gerard Leijdekkers <gerard@eet.nu>
2014-07-02 09:08:21 +0200

    refactor

--- spec/controllers/decks_controller_spec.rb
+++ spec/controllers/decks_controller_spec.rb
@@ -2,21 +2,21 @@ require 'spec_helper'
 
 describe DecksController do
   let(:deck) { create :deck }
+  let(:user) { create :user }
+  
+  before do
+    sign_in user
+  end
   
   describe 'GET #edit' do
     it 'redirects to root when the deck does not belong to current user' do
-      user = create(:user)
-      sign_in user
-      
       get :edit, id: deck.id
       
       response.status.should == 302
     end
     
     it 'loads even when the cardstring is empty' do
-      user = create(:user)
       deck.update_attribute(:user_id, user.id)
-      sign_in user
       
       get :edit, id: deck.id
       
