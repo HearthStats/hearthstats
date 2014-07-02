ef8b0635613c8eb7ea7a5e0ab8799289f970a9c2
Gerard Leijdekkers <gerard@eet.nu>
2014-07-02 08:40:30 +0200

    fix deck_controller_spec

--- spec/controllers/decks_controller_spec.rb
+++ spec/controllers/decks_controller_spec.rb
@@ -2,19 +2,26 @@ require 'spec_helper'
 
 describe DecksController do
   let(:deck) { create :deck }
-
+  
   describe 'GET #edit' do
-
-    context 'when cardstring is blank' do
-      it 'should still load' do
-        user = create(:user)
-        sign_in user
-        get edit_deck_path(deck)
-
-        response.status.should == 200
-      end
+    it 'redirects to root when the deck does not belong to current user' do
+      user = create(:user)
+      sign_in user
+      
+      get :edit, id: deck.id
+      
+      response.status.should == 302
     end
     
+    it 'loads even when the cardstring is empty' do
+      user = create(:user)
+      deck.update_attribute(:user_id, user.id)
+      sign_in user
+      
+      get :edit, id: deck.id
+      
+      response.status.should == 200
+    end
   end
 
 end
