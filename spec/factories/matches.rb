ede3015372a87dabdc6003eeecededf9a187aad0
Jeffrey Tong <trigun0x2@gmail.com>
2014-07-09 15:02:21 -0400

    refactored Match.matches_per_class to use LIST

--- spec/factories/matches.rb
+++ spec/factories/matches.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :match do
     result_id   { [1,2,3].sample     }
     mode_id     { [2,3].sample       }
-    association :klass
-    association :oppclass, factory: :klass
+    klass_id    { (1..9).to_a.sample }
+    oppclass_id { (1..9).to_a.sample }
   end
 end
