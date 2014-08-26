76d20a477a59a2a4db04906af9aff43732f5b757
Jeffrey Tong <trigun0x2@gmail.com>
2014-08-26 10:45:56 -0400

    added localeapp back into system

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 require 'localeapp'
 Localeapp.configure do |config|
   config.api_key = 'oA8Q8t44FAhgH8ZahuZ4gOusLSnKrnFO4U0vvc7lLjwZkSxEDZ'
   config.sending_environments = ['development', 'production', 'test']
 end
+=======
+require 'localeapp/rails'
+
+Localeapp.configure do |config|
+  config.api_key = 'oA8Q8t44FAhgH8ZahuZ4gOusLSnKrnFO4U0vvc7lLjwZkSxEDZ'
+end
+>>>>>>> added localeapp back into system
