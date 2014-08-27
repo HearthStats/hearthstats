f4ca8ed8cfc096fa1702458ac5a7843dcfbf3b46
Jeffrey Tong <trigun0x2@gmail.com>
2014-08-27 10:12:27 -0400

    updated localeapp to notify in prod [skip ci]

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -2,4 +2,8 @@ require 'localeapp'
 Localeapp.configure do |config|
   config.api_key = 'oA8Q8t44FAhgH8ZahuZ4gOusLSnKrnFO4U0vvc7lLjwZkSxEDZ'
   config.sending_environments = ['development', 'production', 'test']
+<<<<<<< HEAD
 end
+=======
+end
+>>>>>>> updated localeapp to notify in prod [skip ci]
