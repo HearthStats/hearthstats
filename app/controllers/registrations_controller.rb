c7597338a90f7feae0434dec282fb6467a04ff4c
Jeffrey Tong <trigun0x2@gmail.com>
2014-08-28 18:31:26 -0400

    added static IP for sendy reg

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -12,7 +12,7 @@ class RegistrationsController < Devise::RegistrationsController
       Profile.create(user_id:resource.id)
 
       begin
-        c = Cindy.new "http://sendy.hearthstats.net", "cGF9DlbzfS0jBooMv5N3"
+        c = Cindy.new "23.253.53.117", "cGF9DlbzfS0jBooMv5N3"
         c.subscribe "aQOe0RrtTXddPhL9p28929MA", resource.email
         c.subscribe "6V763uDbDJuEja62CUwTlthQ", resource.email
       rescue Cindy::AlreadySubscribed
