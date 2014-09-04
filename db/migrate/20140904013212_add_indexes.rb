42b868f3d2d0a40d30a95d8ad255c67d70885729
Jeffrey Tong <trigun0x2@gmail.com>
2014-09-04 11:42:02 -0400

    changed index to up and down

--- db/migrate/20140904013212_add_indexes.rb
+++ db/migrate/20140904013212_add_indexes.rb
@@ -1,5 +1,5 @@
 class AddIndexes < ActiveRecord::Migration
-  def change
+  def up
     # Arena Runs
     add_index :arena_run_cards, :arena_run_id
     add_index :arena_run_cards, :card_id
@@ -18,4 +18,25 @@ class AddIndexes < ActiveRecord::Migration
     add_index :unique_deck_cards, :card_id
 
   end
+
+  def down
+    # Arena Runs
+    add_index :arena_run_cards, :arena_run_id
+    add_index :arena_run_cards, :card_id
+
+    # Cards
+    add_index :cards, :name
+    
+    # Deck
+    add_index :deck_versions, :deck_id 
+
+    # Profile
+    add_index :profiles, :name
+
+    # Unique Deck
+    add_index :unique_deck_cards, :unique_deck_id
+    add_index :unique_deck_cards, :card_id
+
+  end
+
 end
