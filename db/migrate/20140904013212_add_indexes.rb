0f55b128a7eb772a79743452550dee67c55de8ad
Jeffrey Tong <trigun0x2@gmail.com>
2014-09-04 11:50:36 -0400

    added down to remove index

--- db/migrate/20140904013212_add_indexes.rb
+++ db/migrate/20140904013212_add_indexes.rb
@@ -21,21 +21,21 @@ class AddIndexes < ActiveRecord::Migration
 
   def down
     # Arena Runs
-    add_index :arena_run_cards, :arena_run_id
-    add_index :arena_run_cards, :card_id
+    remove_index :arena_run_cards, :arena_run_id
+    remove_index :arena_run_cards, :card_id
 
     # Cards
-    add_index :cards, :name
+    remove_index :cards, :name
     
     # Deck
-    add_index :deck_versions, :deck_id 
+    remove_index :deck_versions, :deck_id 
 
     # Profile
-    add_index :profiles, :name
+    remove_index :profiles, :name
 
     # Unique Deck
-    add_index :unique_deck_cards, :unique_deck_id
-    add_index :unique_deck_cards, :card_id
+    remove_index :unique_deck_cards, :unique_deck_id
+    remove_index :unique_deck_cards, :card_id
 
   end
 
