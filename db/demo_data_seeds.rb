01cf7cf7e7fe637ac69ec77ac69f3e72e00fac22
Bruno Zanchet <bzanchet@gmail.com>
2014-09-07 23:34:34 -0300

    fixing seed data generator to work around protected attributes

--- db/demo_data_seeds.rb
+++ db/demo_data_seeds.rb
@@ -1,14 +1,6 @@
 User.destroy_all
 klasses = Klass.all
 
-# temp add some attributes to be accessible
-class Deck < ActiveRecord::Base
-  attr_accessible :name, :cardstring, :klass_id, :user_id, :active
-end
-
-class Match < ActiveRecord::Base
-  attr_accessible :result_id, :mode_id, :klass_id, :opclass_id, :deck_id, :numturns, :created_at
-end
 
 5.times do |user_count|
   # create user and profile
@@ -18,10 +10,10 @@ end
   # create decks
   (9 + rand(15)).times do |i|
     deck = Deck.new(name:      "demo-deck ##{i}",
-                    klass_id:  klasses.sample.id,
-                    user_id:   user.id,
-                    active:    [true, false].sample,
-                    is_public: [true, false].sample)
+                    klass_id:  klasses.sample.id)
+    deck.user_id = user.id
+    deck.active = [true, false].sample
+    deck.is_public = [true, false].sample
 
     # pick 30 cards
     possible_cards = Card.where("klass_id = ? OR klass_id IS NULL", deck.klass_id)
@@ -48,16 +40,18 @@ end
     result      = 3 if i % 20 == 0
     deck        = user.decks.sample
 
-    match = Match.create(
+    match = Match.new(
       result_id:   result,
       mode_id:     [1,2,3].sample,
       klass_id:    deck.klass_id,
-      oppclass_id: klasses.sample.id,
-      user_id:     user.id,
-      numturns:    4 + rand(15),
-      coin:        [true, false].sample,
       created_at:  rand(100).days.ago
     )
+    match.numturns = 4 + rand(15)
+    match.oppclass_id = klasses.sample.id
+    match.user_id = user.id
+    match.coin = [true, false].sample
+    match.save!
+
     MatchDeck.create(deck_id: deck.id, match_id: match.id)
 
     if match.mode_id == 1
