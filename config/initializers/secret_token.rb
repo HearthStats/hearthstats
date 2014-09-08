94ad72fe5b1bbf49705058db8bc93dfcd12c3d75
Jeffrey Tong <trigun0x2@gmail.com>
2014-09-08 18:44:31 -0400

    fixed untracked files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Hearthstats::Application.config.secret_token = '30d562bdab53ea27f9f1bb6a7eecd0fc4876fde7d610e6fd71bb6b62e2d0eb408392e8b263dd18d942dc0ab430e3c730fec86a6f6b75ac69ddf3f6549678b3b0'
