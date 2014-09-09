946ae2ac851734bd4a2ea02ed5c84b08b2d1828d
Bruno Zanchet <bzanchet@gmail.com>
2014-09-09 18:47:16 -0300

    adding migration to create new index for search

--- db/migrate/20140909164538_add_composite_indexes_on_matches.rb
+++ db/migrate/20140909164538_add_composite_indexes_on_matches.rb
@@ -1,7 +1,11 @@
 class AddCompositeIndexesOnMatches < ActiveRecord::Migration
   def up
+    remove_index :matches, :user_id
+    add_index :matches, [:user_id, :mode_id, :klass_id, :oppclass_id, :coin, :created_at], :name => "index_for_search"
   end
 
   def down
+    remove_index :matches, :name => "index_for_search"
+    add_index :matches, :user_id
   end
 end
