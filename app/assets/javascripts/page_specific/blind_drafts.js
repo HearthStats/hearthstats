45a9d3267631a0af9452d0debc4dcb6d86635d86
Jeffrey Tong <trigun0x2@gmail.com>
2014-09-10 18:52:44 -0400

    implemented auto scroll when cards are flipped

--- app/assets/javascripts/page_specific/blind_drafts.js
+++ app/assets/javascripts/page_specific/blind_drafts.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
   $('#public_match').click(function(){
     $('#opponent').slideToggle(this.unchecked);
   });
+  (function($) {
+        $.fn.goTo = function() {
+                  $('html, body').animate({
+                                scrollTop: $(this).offset().top + 'px'
+                            }, 'fast');
+                          return this; // for chaining...
+                              }
+  })(jQuery);
 });
