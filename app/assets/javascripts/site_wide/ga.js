a05556443f381aafcc4a6b3f8ae3b873de2763c9
Jeffrey Tong <trigun0x2@gmail.com>
2014-10-03 13:31:38 -0400

    Removed Piwik * Removed Piwik from Wel#index and ga.js * Changed notification email for hardcode profile link

--- app/assets/javascripts/site_wide/ga.js
+++ app/assets/javascripts/site_wide/ga.js
@@ -5,14 +5,3 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-45442103-1', 'auto');
 ga('send', 'pageview');
-
-var _paq = _paq || [];
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function() {
-  var u=(("https:" == document.location.protocol) ? "https" : "http") + "://hearthstats.piwikpro.com/";
-  _paq.push(['setTrackerUrl', u+'piwik.php']);
-  _paq.push(['setSiteId', 2]);
-  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
-  g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-})();
