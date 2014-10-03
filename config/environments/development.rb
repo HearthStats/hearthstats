6c2888acfe45e4750ad43f7b4a05d6632d38d805
Jeffrey Tong <trigun0x2@gmail.com>
2014-10-03 13:44:17 -0400

    fixed according to hound style [skip ci]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,7 @@ Hearthstats::Application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
 
   # config.after_initialize do
   #   Bullet.enable = true
