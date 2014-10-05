3b8ccde87f9ccbddddb52e3e5f10765e6093b43c
Jeffrey Tong <trigun0x2@gmail.com>
2014-10-05 15:21:21 -0400

    do nothing is message already read

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,13 @@
 class NotificationsController < ApplicationController
   def note_read
     @note = Notification.find(params[:note_id])
-    current_user.mark_as_read @note
+    respond_to do |format|
+      if @note.receipts.first.is_unread?
+        current_user.mark_as_read @note
+        format.js
+      else
+        # format.js { render :file => "already_read.js" }
+      end
+    end
   end
 end
