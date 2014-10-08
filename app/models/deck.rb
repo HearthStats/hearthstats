deeae2afda65c1d605760388587ac1b2053fb176
Jeffrey Tong <trigun0x2@gmail.com>
2014-10-08 17:29:15 -0400

    fixed tourn deck checking for only false

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -34,7 +34,7 @@ class Deck < ActiveRecord::Base
   end
 
   def self.playable_decks(user_id)
-    Deck.where("user_id = ? AND (is_tourn_deck = ? OR is_tourn_deck = ?)", user_id, false, nil).where("unique_deck_id IS NOT NULL")
+    Deck.where(user_id: user_id, is_tourn_deck:[false, nil]).where("unique_deck_id IS NOT NULL")
   end
   ### INSTANCE METHODS:
 
