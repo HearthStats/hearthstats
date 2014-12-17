8e5c873a49463ca6e3fa38bd44d697fbf5013efb
Jeffrey Tong <trigun0x2@gmail.com>
2014-12-17 14:26:51 -0500

    changed session store to activerecord

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141215054522) do
+ActiveRecord::Schema.define(:version => 20141217192350) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -475,6 +475,16 @@ ActiveRecord::Schema.define(:version => 20141215054522) do
     t.datetime "updated_at", :null => false
     t.string   "name"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "shortened_urls", :force => true do |t|
     t.integer  "owner_id"
