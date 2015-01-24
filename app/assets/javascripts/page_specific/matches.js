1da50b23347be7df6f26a59b448b49aaf0920096
Jeffrey Tong <trigun0x2@gmail.com>
2015-01-23 23:05:16 -0500

    added expand/collapse all button

--- app/assets/javascripts/page_specific/matches.js
+++ app/assets/javascripts/page_specific/matches.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
   $(".turn-details").click(function(){
     turn_num = $(this).attr("id");
-    console.log(turn_num);
     $("#turn" + turn_num).slideToggle();
+  });
+  $("#open-all").click(function(){
+    $(".action").slideToggle();
+    if ($(this).html() == "Expand All"){
+      newVal = "Collapse All"
+    }else{
+      newVal = "Expand All"
+    }
+    $(this).html(newVal);
   });
 });
