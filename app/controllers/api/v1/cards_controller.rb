869ce2b4c61d37b7fcc35b01c34101d6622b58e7
Jeffrey Tong <trigun0x2@gmail.com>
2015-02-06 17:49:38 -0500

    cards api only return non-null cards

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::CardsController < ApplicationController
   # before_filter :validate_userkey
 
   def index
-    @cards = Card.all
+    @cards = Card.where("rarity_id is NOT NULL")
     response = @cards.map {|card| 
       card_j = card.as_json
       card_j["type_id"] = Card::TYPES.invert[card.type_name]
