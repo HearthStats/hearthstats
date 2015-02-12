17c7a57ed3523bf4b2eb528f2b6a74c98be3b9ec
Jeffrey Tong <trigun0x2@gmail.com>
2015-02-12 11:11:47 -0500

    updated card seed data

--- db/seeds.rb
+++
@@ -1,7 +0,0 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-
-%w(class card season match_result mode demo_data).each do |object|
-  require "#{Rails.root}/db/#{object}_seeds.rb"
-end
