7b5dbcc59899d48105e9afdc9c3bfed040d6e16f
Jeffrey Tong <trigun0x2@gmail.com>
2015-03-03 13:38:38 -0500

    updated card page to work

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -7,8 +7,8 @@ class CardsController < ApplicationController
     @cards = Card
 
     #filter by card text
-    @text_filter = CGI.parse(request.query_string)['text'].first
-    if !@text_filter.nil?
+    @text_filter = params['text']
+    if !@text_filter.blank?
       t = Card.arel_table
       @cards = @cards.where(
         t[:name].matches("%" + @text_filter + "%").
@@ -17,39 +17,36 @@ class CardsController < ApplicationController
     end
 
     # filter by card type
-    @type_filter = CGI.parse(request.query_string)['type'].first
-    if !@type_filter.nil? && (Float(@type_filter) rescue false)
-     @cards = @cards.where('type_id = ?', @type_filter)
+    @type_filter = params['type']
+    if !@type_filter.blank?
+     @cards = @cards.where('type_name = ?', @type_filter)
     end
 
     # filter by card class
-    @class_filter = CGI.parse(request.query_string)['class'].first
-    if !@class_filter.nil? && (Float(@class_filter) rescue false)
+    @class_filter = params['class']
+    if !@class_filter.blank?
      @cards = @cards.where('klass_id = ?', @class_filter)
-    else
-      if @class_filter == "neutral"
+    elsif @class_filter == "neutral"
         @cards = @cards.where('klass_id  IS NULL')
-      end
     end
 
     #sort field
     @sort_field = "mana"
-    sort_request = CGI.parse(request.query_string)['sort'].first
-    if !sort_request.nil? && sort_request.match("attack|name|health|mana")
+    sort_request = params['sort']
+    if !sort_request.blank? && sort_request.match("attack|name|health|mana")
       @sort_field = sort_request
     end
 
     # don't show spells if sort by attack or health
     if @sort_field == "attack" || @sort_field == "health"
-      @cards = @cards.where("type_id != ?", 2)
+      @cards = @cards.where("type_name != ?", "Spell")
     end
 
     # sort order
-    @order_filter = CGI.parse(request.query_string)['order'].first
-    if @order_filter.nil? || (@order_filter != "desc" && @order_filter != "asc")
+    @order_filter = params['order']
+    if @order_filter.blank? || (@order_filter != "desc" && @order_filter != "asc")
      @order_filter = "asc"
     end
     @cards = @cards.order(@sort_field + ' ' + @order_filter.upcase + ", name ASC")
-
   end
 end
