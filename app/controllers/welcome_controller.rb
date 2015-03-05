893adf940cc96f46328f7ef940dbf9c40c46fc8e
Jeffrey Tong <trigun0x2@gmail.com>
2015-03-05 12:44:25 -0500

    fixed home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -28,7 +28,7 @@ class WelcomeController < ApplicationController
       rank_percent = {}
       rank_class.each do |rank, counts|
         tot = counts.map{|w|w[1]}.reduce(:+)
-        percent = count.to_f/tot*100.to_s[/\d+\.\d{#{n}}/].to_f
+        percent = counts.to_f/tot*100.to_s[/\d+\.\d{#{n}}/].to_f
         klass_percent = counts.map {|klass, count| [klass, percent]}
         rank_percent[rank] = klass_percent
       end
