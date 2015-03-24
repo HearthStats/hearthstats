d8bf78c150f9440fcb5346ffcece6a97fe23c232
Jeffrey Tong <trigun0x2@gmail.com>
2015-03-24 16:02:15 -0400

    changed ads js

--- app/assets/javascripts/site_wide/ad.js
+++ app/assets/javascripts/site_wide/ad.js
@@ -2,7 +2,7 @@
 //   var iframe = document.getElementById('topad');
 //   iframe.src = iframe.src;
 // },150000);
-window.setInterval(function() {
-  var iframe = document.getElementById('footad');
-  iframe.src = iframe.src;
-},60000);
+// window.setInterval(function() {
+//   var iframe = document.getElementById('footad');
+//   iframe.src = iframe.src;
+// },60000);
