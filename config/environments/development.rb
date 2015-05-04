6da3e5d02b2a741758d07a6121bfa57768e26644
Michelle Vo <msongvo@gmail.com>
2015-05-04 16:16:50 -0400

    Change icon location in public show page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Hearthstats::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
   config.assets.digest = false
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
