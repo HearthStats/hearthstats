4a13e74dc13927488c45d94ad94020096e0d119c
Jeffrey Tong <trigun0x2@gmail.com>
2015-05-04 12:14:15 -0400

    added commanding shout

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -2,6 +2,5 @@ class StreamsController < ApplicationController
   def index
     twitch_response = HTTParty.get('https://api.twitch.tv/kraken/search/streams?limit=50&q=hearthstone&client_id=5p5btpott5bcxwgk46azv8tkq49ccrv')
     @streams = twitch_response['streams'].paginate(page: params[:page], per_page: 12)
-    @featured_streams = Stream.get_featured_streamers
   end
 end
