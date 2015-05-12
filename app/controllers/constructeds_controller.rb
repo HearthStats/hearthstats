e1a859490a156c3c4938ddcdd9a5304f79d7f492
Jeffrey Tong <trigun0x2@gmail.com>
2015-05-12 09:08:37 -0400

    fixed error in con_controller

--- app/controllers/constructeds_controller.rb
+++ app/controllers/constructeds_controller.rb
@@ -141,7 +141,7 @@ class ConstructedsController < ApplicationController
       if @constructed.save
         MatchDeck.create(deck_id: deck.id, 
                          match_id: @constructed.id,
-                         deck_version_id: deck.
+                         deck_version_id: deck.current_version.try(:id)
                         )
         delete_deck_cache!(deck)
         format.html { redirect_to constructeds_path, notice: 'Constructed was successfully created.' }
