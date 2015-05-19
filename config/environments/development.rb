0253824a03ca423e66e9b6d37967458fe092a5ba
Jeffrey Tong <trigun0x2@gmail.com>
2015-05-19 16:32:05 -0400

    updated homepage for archtype graph

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,12 +13,12 @@ Hearthstats::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
-  config.cache_store = :null_store
-  config.serve_static_assets = true
+  # config.action_controller.perform_caching = false
+  # config.cache_store = :null_store
+  # config.serve_static_assets = true
   # Caching testing:
-  # config.action_controller.perform_caching = true
-  # config.cache_store = :dalli_store
+  config.action_controller.perform_caching = true
+  config.cache_store = :dalli_store
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
