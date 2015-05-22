0e3db6204b7b796270ef95de8ed3626b09074860
Jeffrey Tong <trigun0x2@gmail.com>
2015-05-22 17:08:09 -0400

    updated welcome cache calculation method

--- app/controllers/api/stats/constructeds_controller.rb
+++ app/controllers/api/stats/constructeds_controller.rb
@@ -3,7 +3,7 @@ class Api::Stats::ConstructedsController < ApplicationController
 
   def class_wr
     classconrate = Rails.cache.fetch('api#stats#class_wr', expires_in: 5.seconds) do
-      con_matches = Match.last(1000)
+      con_matches = Match.where(mode_id: 3).last(1000)
       classconrate = [["Class", "Winrate", { role: 'style' }]]
       grouped_matches = con_matches.group_by{|m| m.klass_id}
       wins = {}
@@ -16,7 +16,7 @@ class Api::Stats::ConstructedsController < ApplicationController
       wins.each_pair do |klass_id, win|
         classconrate << [ Klass::LIST[klass_id], (win.to_f/tot_games[klass_id] * 100).round(2), Klass::COLORS[klass_id]]
       end
-      
+
       classconrate.sort_by {|class_name, wr| class_name }
     end
     
@@ -26,7 +26,7 @@ class Api::Stats::ConstructedsController < ApplicationController
 
   def global_wr
     wr = Rails.cache.fetch('api#stats#global_wr', expires_in: 5.seconds) do
-      get_array_wr(Match.last(100))
+      get_array_wr(Match.where(mode_id: 3).last(100))
     end
     render json: wr
   end
