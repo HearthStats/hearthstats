6f5c12e1574a36cda1f17d67dc4c994476c656a2
Jeffrey Tong <trigun0x2@gmail.com>
2015-05-25 17:28:11 -0400

    deck show sorts by mana then name now

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -253,13 +253,13 @@ class Deck < ActiveRecord::Base
     cardstring_array = cardstring_as_array
 
     arr = []
-    cards = Card.where("id IN (?)", cardstring_array.map {|e| e[0]}).order("mana, name").all
+    cards = Card.find(cardstring_array.map {|e| e[0]}).sort_by{ |card| [ card.mana, card.name] }
     cards.each do |card|
-      element = cardstring_array.detect {|c| c[0].to_i == card.id }
-      arr << [card, element[1]]
+      quantity = cardstring_array.detect {|c| c[0].to_i == card.id }[1]
+      arr << [card, quantity]
     end
 
-    arr.sort_by { |c| c[0].mana }
+    arr
   end
 
   private
