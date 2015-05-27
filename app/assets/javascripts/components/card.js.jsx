7b095b743ab4f8ebb6b493645f346a7cefdfd318
Michelle Vo <msongvo@gmail.com>
2015-05-27 15:44:13 -0400

    complete card functionality
    - click to add card
    - click to remove card
    - 30 card limit
    - slight card expand on hover

--- app/assets/javascripts/components/card.js.jsx
+++ app/assets/javascripts/components/card.js.jsx
@@ -2,7 +2,7 @@ var Card = React.createClass({
 	render: function(){ 
 		cn = this.props.card.name.trim().replace(/[^a-zA-Z0-9-\s-\']/g, '').replace(/[^a-zA-Z0-9-]/g, '-').replace(/--/g, '-').toLowerCase();
 		return (
-			<img src={"/assets/cards/" + cn +".png"} className="deckbuilder-img" onClick={this.handleClick}/>
+			<img src={"/assets/cards/" + cn +".png"} className="deckbuilder-img expandHover" onClick={this.handleClick}/>
 		);
 	},
 	handleClick: function(event) {
