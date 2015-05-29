8b9a3657dcd4aa3e582d7dff10b51a6b0e109b85
Jeffrey Tong <trigun0x2@gmail.com>
2015-05-29 00:17:36 -0400

    fixed sort by in public decks

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -26,7 +26,7 @@ class DecksController < ApplicationController
     @q.unique_deck_created_at_gteq = 1.week.ago unless params[:q]
 
     @decks = @q.result
-    @decks = @decks.order("unique_decks.#{sort_by} #{direction}")
+    @decks = @decks.order("#{sort_by} #{direction}")
     @decks = @decks.paginate(page: params[:page], per_page: params[:items])
 
     unless current_user.nil?
@@ -463,11 +463,9 @@ class DecksController < ApplicationController
   end
 
   def sort_by
-    return 'num_users' unless params[:sort]
-
     sort = (Deck.column_names + UniqueDeck.column_names).include?(params[:sort]) ? params[:sort] : 'num_users'
 
-    sort =  %w(created_at name).include?(sort) ? "decks.#{sort}" : sort
+    sort =  %w(created_at name).include?(sort) ? "decks.#{sort}" : "unique_decks.#{sort}"
   end
 
   def direction
