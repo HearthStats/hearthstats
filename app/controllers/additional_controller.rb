be96c5e725a66dc6a06f67d3f02a3339f3f1fc6a
Jeffrey Tong <trigun0x2@gmail.com>
2015-06-15 13:36:59 -0400

    updated uploader page

--- app/controllers/additional_controller.rb
+++ app/controllers/additional_controller.rb
@@ -18,9 +18,9 @@ class AdditionalController < ApplicationController
     @urls = Rails.cache.fetch('uploader_url', expires_in: 2.days) do
       urls = Hash.new
       win_git_response = HTTParty.get('https://api.github.com/repos/Epix37/Hearthstone-Deck-Tracker/releases?per_page=1', headers: { "User-Agent" => "HearthStats"})
-      osx_git_response = HTTParty.get('https://api.github.com/repos/HearthStats/HearthStats.net-Uploader/releases?per_page=1', headers: { "User-Agent" => "HearthStats"})
-      urls["osx"] = osx_git_response[0]["assets"][0]["browser_download_url"]
-      urls["windows"] = win_git_response[0]["assets"][0]["browser_download_url"]
+      osx_git_response = HTTParty.get('https://api.github.com/repos/bmichotte/HSTracker/releases?per_page=1', headers: { "User-Agent" => "HearthStats"})
+      urls["osx"] = "asd"
+      urls["windows"] = ""
       urls
     end
     render layout: "prem_dash"
