cbd1d86c260ae7dd7909b24db6db5bddbde35823
Michelle Vo <msongvo@gmail.com>
2015-06-19 13:25:13 -0400

    image loading deferral
    - also removed extraneous instance variables

--- app/assets/javascripts/components/card.js.jsx
+++ app/assets/javascripts/components/card.js.jsx
@@ -1,11 +1,20 @@
 var Card = React.createClass({
-	render: function(){ 
-		cn = this.props.card.name.trim().replace(/[^a-zA-Z0-9-\s-\']/g, '').replace(/[^a-zA-Z0-9-]/g, '-').replace(/--/g, '-').toLowerCase();
+	getInitialState: function(){
+		var cn = this.props.card.name.trim().replace(/[^a-zA-Z0-9-\s-\']/g, '').replace(/[^a-zA-Z0-9-]/g, '-').replace(/--/g, '-').toLowerCase();
 		if(cn=="si7-agent"){ 
 			cn = "si-7-agent";
 		}
+		return{
+			hasLoaded: false,
+			imgSrc: "/assets/cards/" + cn +".png"
+		}
+	},
+	componentDidMount: function(){
+		this.setState({ hasLoaded: true });
+	},
+	render: function(){ 
 		return (
-			<img src={"/assets/cards/" + cn +".png"} className={"deckbuilder-img " + this.props.cName} onClick={this.handleClick}/>
+			<img src={this.state.hasLoaded ? this.state.imgSrc : "/assets/blind_draft/deckbuilder-card-back.png"} className={"deckbuilder-img " + this.props.cName} onClick={this.handleClick}/>
 		);
 	},
 	handleClick: function(event) {
