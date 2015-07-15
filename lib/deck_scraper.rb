dcc15b0b4d29f1f5fb2c48c74e100db4d2a549c8
Michelle Vo <msongvo@gmail.com>
2015-07-15 11:47:50 -0400

    import decks from htd
    - creates new deck & user if needed
    - currently attaches to first user with same name (might be user that
    already has the same name)
    - updates unique deck stats

--- lib/deck_scraper.rb
+++ lib/deck_scraper.rb
@@ -20,17 +20,17 @@ class DeckScraper
           @cardlist << card.text
         end
         unless @cardlist.length == 0
-          @tournament = dp.search("div#leftbar div.headbar div").first.text.strip
-          @klass = dp.search("td span.midlarge span").first.text
+          tournament = dp.search("div#leftbar div.headbar div").first.text.strip
+          klass = dp.search("td span.midlarge span").first.text
           text = dp.search("div#center div.headbar div").first.content
-          @name = text[(text.index("-") + 2)..(text.rindex("-") - 1)]
-          @author = text[(text.rindex("-") + 2)..-1]
-          @cardlist = @cardlist.join("\r\n")
+          name = text[(text.index("-") + 2)..(text.rindex("-") - 1)]
+          author = text[(text.rindex("-") + 2)..-1]
+          cardlist = @cardlist.join("\r\n")
           deck_obj = {}
-          deck_obj[:name] = @name + ": " + @tournament
-          deck_obj[:user] = @author
-          deck_obj[:klass] = @klass
-          deck_obj[:cards] = @cardlist
+          deck_obj[:name] = name + ": " + tournament
+          deck_obj[:user] = author
+          deck_obj[:klass] = klass
+          deck_obj[:cards] = cardlist
           output_decks << deck_obj
         end
       end
