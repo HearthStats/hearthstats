60e249e7aa733d26c07de90daa9a1c100266c714
Colin Jackson <colin.s.jackson90@gmail.com>
2015-08-21 01:50:34 -0400

    Improve and verify new match id-assigning logic

--- app/jobs/matches/create_new_matches_job.rb
+++ app/jobs/matches/create_new_matches_job.rb
@@ -1,11 +1,10 @@
 module MatchJobs
   CreateNewMatchesJob = Struct.new(:matches_params, :deck, :user_id) do
     def perform
-      initial_id = Match.last ? Match.last.id : 0
-      Match.connection.insert Match.generate_mass_insert_sql(matches_params, deck.klass_id, user_id)
+      first_id = Match.connection.insert Match.generate_mass_insert_sql(matches_params, deck.klass_id, user_id)
 
       matches_params.each_with_index do |match_params, i|
-        match_params[:id] = initial_id + i + 1
+        match_params[:id] = first_id + i
       end
 
       MatchDeck.connection.insert MatchDeck.generate_mass_insert_sql(matches_params, deck.id)
@@ -13,6 +12,7 @@ module MatchJobs
       ranked_params = matches_params.select do |match_params|
         match_params[:mode] == "Ranked" && match_params[:ranklvl]
       end
+
       unless ranked_params.empty?
         MatchRank.connection.insert MatchRank.generate_mass_insert_sql(ranked_params)
       end
