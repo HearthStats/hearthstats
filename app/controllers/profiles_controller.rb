c059cc5b4e3c083bd9abc60ea0131b2f11f340bb
Jeffrey Tong <trigun0x2@gmail.com>
2015-10-26 20:38:03 -0700

    sped up profiles contorller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -40,7 +40,7 @@ class ProfilesController < ApplicationController
       return redirect_to root_url, alert: "Guests profiles cannot be accessed"
     end
 
-    matches = Match.where(user_id: @user.id)
+    matches = Match.where({user_id: @user.id, season_id: current_season}).all
     @userkey = @user.get_userkey
     @profile = @user.profile
     impressionist(@profile)
@@ -50,30 +50,36 @@ class ProfilesController < ApplicationController
     @recent_matches = matches.last(6).reverse
 
     # Overall win rates
-    arena_matches = matches.where(mode_id: 1)
-    @overallarena= get_win_rate(arena_matches, true)
-    con_matches = matches.where(mode_id: 3)
-    @overallcon = get_win_rate(con_matches, true)
+    arena_matches = matches.select {|match| match.mode_id == 1}
+    @overallarena= get_array_wr(arena_matches, true)
+    con_matches = matches.select {|match| match.mode_id == 3}
+    @overallcon = get_array_wr(con_matches, true)
 
     @conwins   = Match.winrate_per_day_cumulative(con_matches, 10)
     @arenawins = Match.winrate_per_day_cumulative(arena_matches, 10)
 
     # Determine Constructed Class Win Rates
     @classconrate = Array.new
-    matches = con_matches.group(:klass_id)
-    wins = matches.where(result_id: 1).count
-    tot_games = matches.count
-    wins.each_pair do |klass_id, win|
-      @classconrate << [ (win.to_f/tot_games[klass_id] * 100).round(2), "#{Klass::LIST[klass_id]}<br/>#{tot_games[klass_id]} Games", klass_id]
+    klass_matches = con_matches.group_by {|m| m.klass_id}
+    klass_matches.each do |klass_id, klass_m|
+      wins = klass_m.select {|m| m.result_id == 1}.count
+      tot = klass_m.count
+
+      wr = (wins.to_f/tot * 100 ).round(2)
+
+      @classconrate << [wr, "#{Klass::LIST[klass_id]}<br/>#{tot} Games", klass_id]
     end
 
     # Determine Arena Class Win Rates
     @classarenarate = Array.new
-    matches = arena_matches.group(:klass_id)
-    wins = matches.where(result_id: 1).count
-    tot_games = matches.count
-    wins.each_pair do |klass_id, win|
-      @classarenarate << [ (win.to_f/tot_games[klass_id] * 100).round(2), "#{Klass::LIST[klass_id]}<br/>#{tot_games[klass_id]} Games", klass_id]
+    klass_matches = arena_matches.group_by {|m| m.klass_id}
+    klass_matches.each do |klass_id, klass_m|
+      wins = klass_m.select {|m| m.result_id == 1}.count
+      tot = klass_m.count
+
+      wr = (wins.to_f/tot * 100 ).round(2)
+
+      @classarenarate << [wr, "#{Klass::LIST[klass_id]}<br/>#{tot} Games", klass_id]
     end
 
     # User's Highest Winning Decks
