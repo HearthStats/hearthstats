c5771d8fdbdad5117732ab0a39fea13e999bc9b6
Jeffrey Tong <trigun0x2@gmail.com>
2015-11-03 17:39:03 -0800

    fixed deck error

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -174,11 +174,11 @@ class Deck < ActiveRecord::Base
   end
 
   def update_user_stats!(match)
-    self.user_num_matches += 1
+    self.user_num_matches = self.user_num_matches.to_i + 1
     if match.result_id == 1
-      self.user_num_wins  += 1
+      self.user_num_wins = user_num_wins.to_i + 1
     elsif match.result_id == 2
-      self.user_num_losses  += 1
+      self.user_num_losses = user_num_losses.to_i + 1
     end
     self.user_winrate     = user_num_matches > 0 ? (user_num_wins.to_f / user_num_matches) * 100 : 0
 
