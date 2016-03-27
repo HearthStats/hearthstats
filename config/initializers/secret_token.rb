e347e4135b6209c6c3eb36681673392896786ec2
Jeffrey Tong <trigun0x2@gmail.com>
2016-03-27 10:43:31 -0700

    change to cookie session store

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Hearthstats::Application.config.secret_token = 'ec061f5ba1404ea0dfa985a1412e5cebed872df9fbfc70fca1b1bc83d2ccbd75046574b747661e6f14aa7026fecc31c0be97709a0b2e6421d0448b86ff0893cf'
+Hearthstats::Application.config.secret_token = ENV['SECRET_TOKEN']
 #ActiveRecordQueryTrace.enabled = true
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Hearthstats::Application.config.session_store :active_record_store
+Hearthstats::Application.config.session_store :cookie_store, key: '_hs_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
